baec91c9ea8ec6ce93e6be384bd086d3e4a49331
mizraim17 <mizraim.martinez@gmail.com>
2022-09-27 00:21:22 -0500

    add gitignore

--- main.js
+++ main.js
@@ -57,221 +57,6 @@ let oportunities = [false, false, false];
 
 //Generate prompr for inputs values and validate if the user win or lose
 
-let menu = (asseMurder) => {
-	let coins = 0;
-
-	let arrWitMurd = asseMurder[0];
-	let copyArrWitMurd = asseMurder[0];
-
-	for (element in arrWitMurd) {
-		console.log(
-			`original "${arrWitMurd[element].id}  ${arrWitMurd[element].nombre}`
-		);
-	}
-	let numAssesin = parseInt(doRandom(arrWitMurd));
-
-	let numWeapon = parseInt(doRandom(weaponsArray));
-	let numRoom = parseInt(doRandom(roomsArray));
-	let gameWin = 0;
-
-	//Me ayuda a encontrar el misterio y ganar
-	// alert(`numAssesin ${numAssesin}`);
-	// alert(`numWeapon ${numWeapon}`);
-	// alert(`numnumRoomWeapon ${numRoom}`);
-
-	do {
-		// alert(`oportunities ${oportunities}`);
-		// alert(`coins ${coins}`);
-
-		let selectUser = parseInt(
-			prompt(
-				`"Bienvenido a Clue version the Office mataron a: "
-				${
-					asseMurder[1]
-				} "es tu deber adivinar en 5 oportunidades, quién lo mato, con que lo mató y donde lo mató\n\n "${
-					oportunities[0] == true
-						? "Asesino adivinado \n"
-						: "1.-Adivinar el nombre del asesino\n"
-				} ${
-					oportunities[1] == true ? "Arma adivinada\n" : "2. Adivinar Arma\n"
-				}	${
-					oportunities[2] == true
-						? "Habitación adivinada"
-						: " 3. Adivinar Habitación\n"
-				}  `
-			)
-		);
-
-		for (i = 0; i < 3; i++) {
-			if (oportunities[i] === true) {
-				gameWin++;
-			}
-		}
-
-		switch (selectUser) {
-			case 1:
-				let instructions = "",
-					optiAssesin = 0;
-				counter = 0;
-
-				console.log("numero asesino", asseMurder[0]);
-
-				do {
-					instructions = generateInstru(arrWitMurd);
-
-					optiAssesin = parseInt(prompt(`${instructions}`));
-					coins++;
-					gene;
-					console.log("numAssesin", numAssesin);
-					console.log("optiAssesin", optiAssesin);
-
-					if (copyArrWitMurd[numAssesin].id !== optiAssesin) {
-						for (element in arrWitMurd) {
-							console.log(
-								`antes cut "${arrWitMurd[element].id}  ${arrWitMurd[element].nombre}`
-							);
-						}
-
-						matchSuspects([arrWitMurd[element].id]);
-
-						arrWitMurd = arrWitMurd.filter((item) => item.id !== optiAssesin);
-
-						for (element in arrWitMurd) {
-							console.log(
-								`despues cut "${arrWitMurd[element].id}  ${arrWitMurd[element].nombre}`
-							);
-						}
-
-						paintingCharacters(arrWitMurd);
-						console.log("entro a borrar arrWitMurd", arrWitMurd);
-					}
-
-					// alert(`coins=${coins}`);
-				} while (numAssesin !== optiAssesin && coins < 7);
-
-				if (optiAssesin === numAssesin) {
-					alert(`Correcto fue ${copyArrWitMurd[optiAssesin].nombre}`);
-
-					// matchSuspects([numAssesin]);
-					selectUser = 0;
-					oportunities[0] = true;
-				} else if (coins === 5) {
-					alert(`Perdiste fue ${arrWitMurd[numAssesin].nombre}`);
-					selectUser = "s";
-				}
-
-				break;
-
-			case 2:
-				const TXT2 = "Adivina el arma del asesino";
-				let instructions2 = "",
-					optiWeapons = 0;
-				counter2 = 0;
-
-				for (element in weaponsArray) {
-					instructions2 =
-						instructions2 + `${counter2}.- ${weaponsArray[element]}\n`;
-					counter2++;
-				}
-
-				instructions2 + TXT2;
-
-				do {
-					optiWeapons = parseInt(prompt(`${instructions2}`));
-					coins++;
-				} while (numWeapon !== optiWeapons && coins < 7);
-
-				if (numWeapon === optiWeapons) {
-					alert(`Ganaste el arma usada fue ${weaponsArray[numWeapon]}`);
-					selectUser = "s";
-					oportunities[1] = true;
-				} else if (coins === 7) {
-					alert(`Perdiste el arma usada era ${weaponsArray[numWeapon]}`);
-					selectUser = "s";
-				}
-
-				break;
-
-			case 3:
-				const TXT3 = "Adivina donde fue el asesinato";
-				let instructions3 = "",
-					optiPlace = 0;
-				counter3 = 0;
-
-				for (element in roomsArray) {
-					instructions3 =
-						instructions3 + `${counter3}.- ${roomsArray[element]}\n`;
-					counter3++;
-				}
-
-				instructions3 + TXT3;
-
-				do {
-					optiPlace = parseInt(prompt(`${instructions3}`));
-
-					coins++;
-				} while (numRoom !== optiPlace && coins < 7);
-
-				if (numRoom === optiPlace) {
-					alert(`Ganaste el lugar es la ${roomsArray[numRoom]}`);
-					selectUser = "s";
-					oportunities[2] = true;
-				} else if (coins === 5) {
-					alert(`Perdiste el lugar era ${roomsArray[numRoom]}`);
-					selectUser = "s";
-				}
-
-				break;
-
-			default:
-				alert("Opción inválida");
-				break;
-
-			case "s":
-				break;
-		}
-
-		// alert(`gameWin ${gameWin}`);
-		alert(`coins ${coins}`);
-	} while (selectUser !== "s" && coins !== 7 && gameWin !== 3);
-
-	if (coins === 7) {
-		let text_lose = `Perdiste el juego, fue  \n <b> ${copyArrWitMurd[numAssesin].nombre} </b> mato a
-			<b>  ${asseMurder[1]} </b>  con la <b> ${weaponsArray[numWeapon]} </b>   en <b>   ${roomsArray[numRoom]} </b> `;
-
-		labelModal = document.getElementById("exampleModalLabel");
-		labelModal.innerHTML = "GAMEEEEE OVERRRRR";
-
-		getModal = document.getElementById("exampleModal");
-
-		txtModal = document.getElementById("txt-modal");
-
-		getModal.classList.add("show");
-		getModal.classList.add("bg-danger");
-
-		getModal.style.display = "block";
-
-		txtModal.innerHTML = text_lose;
-	} else if (gameWin == 3) {
-		let text_win = `Felicidades ganaste el juego, fue <b>  \n ${arrWitMurd[numAssesin].nombre} </b>   mato a
-			<b>  ${asseMurder[1]} </b> con la <b> ${weaponsArray[numWeapon]} </b>  en <b> ${roomsArray[numRoom]} </b> `;
-
-		labelModal = document.getElementById("exampleModalLabel");
-		labelModal.innerHTML = "WIIIIIN";
-
-		getModal = document.getElementById("exampleModal");
-
-		txtModal = document.getElementById("txt-modal");
-
-		getModal.classList.add("show");
-		getModal.classList.add("bg-success");
-
-		getModal.style.display = "block";
-
-		txtModal.innerHTML = text_win;
-	}
-};
-
 //init
 
 let initElements = () => {
@@ -359,12 +144,12 @@ let showAssasin = () => {
 
 	containerCharacters.innerHTML = "";
 
-	column.className = "col-md-4 mt-3";
+	column.className = "col-md-3 mt-3";
 	column.id = `character-${suspectsArray[id_assasin].id}`;
-	column.innerHTML = `<div class="card" style="width: 18rem;">
+	column.innerHTML = `<div class="card card-special"  >
   <img src="./characters/${suspectsArray[id_assasin].name_image}.png" id="imgId-${suspectsArray[id_assasin].id}" class="card-img-top " alt="...">
   <div class="card-body">
-    <h5 class="card-title">${suspectsArray[id_assasin].nombre}</h5>
+    <pclass="card-title">${suspectsArray[id_assasin].nombre}</p>
     <a href="#" id="btn-possAssesin-${suspectsArray[id_assasin].id}" class="btn btn-primary">tú fuiste</a>
   </div>
 </div>`;
@@ -388,12 +173,12 @@ let showWeapon = () => {
 		weaponsArray[id_weapon].nombre
 	);
 
-	column.className = "col-md-4 mt-3";
+	column.className = "col-md-3 mt-3";
 	column.id = `weapon-${weaponsArray[id_weapon].id}`;
-	column.innerHTML = `<div class="card" style="width: 18rem;">
+	column.innerHTML = `<div class="card card-special" >
   <img src="./weapons/${weaponsArray[id_weapon].name_image}.jpg" id="imgId-${weaponsArray[id_weapon].id}" class="card-img-top " alt="...">
   <div class="card-body">
-    <h5 class="card-title">${weaponsArray[id_weapon].nombre}</h5>
+    <pclass="card-title">${weaponsArray[id_weapon].nombre}</p>
     <a href="#" id="btn-poss-weapon-${weaponsArray[id_weapon].id}" class="btn btn-primary">con está arma</a>
   </div>
 </div>`;
@@ -407,12 +192,12 @@ let paintingCharacters = (arrSuspects) => {
 	arrSuspects.forEach((character) => {
 		let column = document.createElement("div");
 
-		column.className = "col-md-4 mt-3";
+		column.className = "col-md-3 mt-3";
 		column.id = `character-${character.id}`;
-		column.innerHTML = `<div class="card" style="width: 18rem;">
+		column.innerHTML = `<div class="card card-special" >
   <img src="./characters/${character.name_image}.png" id="imgId-${character.id}" class="card-img-top " alt="...">
   <div class="card-body">
-    <h5 class="card-title">${character.nombre}</h5>
+    <pclass="card-title">${character.nombre}</p>
     <a href="#" id="btn-possAssesin-${character.id}" class="btn btn-primary">tú fuiste</a>
   </div>
 </div>`;
@@ -435,12 +220,12 @@ let paintingWeapons = (arrWeapons) => {
 		// console.log("weapon id----------------------", weapon.id);
 		// console.log("weapon name----------------------", weapon.nombre);
 
-		column.className = "col-md-4 mt-3";
+		column.className = "col-md-3 mt-3";
 		column.id = `weapon-${weapon.id}`;
-		column.innerHTML = `<div class="card" style="width: 18rem;">
+		column.innerHTML = `<div class="card card-special"  >
   	<img src="./weapons/${weapon.name_image}.jpg" id="imgId-${weapon.id}" class="card-img-top " alt="...">
   	<div class="card-body">
-    <h5 class="card-title">${weapon.nombre}</h5>
+    <pclass="card-title">${weapon.nombre}</p>
     <a href="#" id="btn-poss-weapon-${weapon.id}" class="btn btn-primary">tú fuiste</a>
 		</div>
 		</div>`;
@@ -564,8 +349,6 @@ let main = () => {
 	paintingCharacters(arrToPlay[0]);
 
 	play(arrToPlay[0]);
-
-	// blur();
 };
 
 main();
